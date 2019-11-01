3354beb48cfb3820899bcfadaf73e8b8a243a249
stefanprodan <stefan.prodan@gmail.com>
2019-11-02 00:17:51 +0200

    Refactor command flags - add klog verbosity flag to the root cmd - remove sub commands

--- Makefile
+++ Makefile
@@ -11,12 +11,8 @@ test:
 	go test -v -race ./...
 
 run:
-	go run cmd/appmesh-gateway/*.go kubernetes --kubeconfig=$$HOME/.kube/config \
-		--port-name=http
-
-appmesh:
-	go run cmd/appmesh-gateway/*.go appmesh --kubeconfig=$$HOME/.kube/config \
-		--gateway-mesh=appmesh --gateway-name=gateway --gateway-namespace=appmesh-gateway
+	go run cmd/appmesh-gateway/*.go --kubeconfig=$$HOME/.kube/config -v=4 \
+	--gateway-mesh=appmesh --gateway-name=gateway --gateway-namespace=appmesh-gateway
 
 envoy:
 	envoy -c envoy.yaml -l info
--- cmd/appmesh-gateway/main.go
+++ cmd/appmesh-gateway/main.go
@@ -1,22 +1,37 @@
 package main
 
 import (
+	"context"
+	goflag "flag"
 	"fmt"
 	"os"
 	"strings"
 
 	"github.com/spf13/cobra"
+	flag "github.com/spf13/pflag"
+	"k8s.io/client-go/dynamic"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog"
+
+	"github.com/stefanprodan/appmesh-gateway/pkg/discovery"
+	"github.com/stefanprodan/appmesh-gateway/pkg/envoy"
+	"github.com/stefanprodan/appmesh-gateway/pkg/server"
+	"github.com/stefanprodan/appmesh-gateway/pkg/signals"
 )
 
 const VERSION = "0.3.0"
 
 var (
-	masterURL  string
-	kubeConfig string
-	port       int
-	namespace  string
-	ads        bool
-	optIn      bool
+	masterURL        string
+	kubeConfig       string
+	port             int
+	namespace        string
+	ads              bool
+	optIn            bool
+	gatewayMesh      string
+	gatewayName      string
+	gatewayNamespace string
 )
 
 func init() {
@@ -27,19 +42,75 @@ func init() {
 	pf.BoolVarP(&ads, "ads", "a", true, "ADS flag forces all Envoy resources to be explicitly named in the request.")
 	pf.StringVarP(&namespace, "namespace", "n", "", "Namespace to watch for Kubernetes objects, a blank value means all namespaces.")
 	pf.BoolVarP(&optIn, "opt-in", "", false, "When enabled only services with the 'expose' annotation will be discoverable.")
-
+	pf.StringVarP(&gatewayMesh, "gateway-mesh", "", "", "App Mesh mesh that this gateway belongs to.")
+	cobra.MarkFlagRequired(pf, "gateway-mesh")
+	pf.StringVarP(&gatewayName, "gateway-name", "", "", "Gateway Kubernetes service name.")
+	cobra.MarkFlagRequired(pf, "gateway-name")
+	pf.StringVarP(&gatewayNamespace, "gateway-namespace", "", "", "Gateway Kubernetes namespace.")
+	cobra.MarkFlagRequired(pf, "gateway-namespace")
 }
 
 var rootCmd = &cobra.Command{
-	Use:     "kxds",
+	Use:     "appmesh-gateway",
+	Long:    `appmesh-gateway is responsible for exposing services outside the mesh.`,
 	Version: VERSION,
+	RunE:    run,
 }
 
 func main() {
+	flag.CommandLine.Parse([]string{})
+	pf := rootCmd.PersistentFlags()
+	addKlogFlags(pf)
+
 	rootCmd.SetArgs(os.Args[1:])
 	if err := rootCmd.Execute(); err != nil {
 		e := err.Error()
 		fmt.Println(strings.ToUpper(e[:1]) + e[1:])
 		os.Exit(1)
 	}
+}
+
+func run(cmd *cobra.Command, args []string) error {
+	cfg, err := clientcmd.BuildConfigFromFlags(masterURL, kubeConfig)
+	if err != nil {
+		klog.Fatalf("error building kubeconfig: %v", err)
+	}
+
+	client, err := dynamic.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("error building kubernetes client: %v", err)
+	}
+
+	stopCh := signals.SetupSignalHandler()
+	ctx := context.Background()
+	cache := envoy.NewCache(ads)
+	snapshot := envoy.NewSnapshot(cache)
+
+	klog.Infof("starting xDS server on port %d", port)
+	srv := server.NewServer(port, cache)
+	go srv.Serve(ctx)
+
+	klog.Info("waiting for Envoy to connect to the xDS server")
+	srv.Report()
+
+	klog.Info("starting App Mesh discovery workers")
+	vsManager := discovery.NewVirtualServiceManager(client, optIn)
+	vnManager := discovery.NewVirtualNodeManager(client, gatewayMesh, gatewayName, gatewayNamespace)
+	kd := discovery.NewController(client, namespace, snapshot, vsManager, vnManager)
+	kd.Run(2, stopCh)
+
+	return nil
+}
+
+func addKlogFlags(fs *flag.FlagSet) {
+	local := goflag.NewFlagSet(os.Args[0], goflag.ExitOnError)
+	klog.InitFlags(local)
+	local.VisitAll(func(fl *goflag.Flag) {
+		fl.Name = normalizeFlag(fl.Name)
+		fs.AddGoFlag(fl)
+	})
+}
+
+func normalizeFlag(s string) string {
+	return strings.Replace(s, "_", "-", -1)
 }
